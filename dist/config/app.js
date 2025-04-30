eca1f7dfc041027ab2577dfa39f8cb434b3ab638
Celeste <celesn3045@gmail.com>
2025-04-30 20:59:05 -0300

    🔥 Remove .env and dist from version control

--- dist/config/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appConfig = void 0;
-exports.appConfig = {
-    JWT_SECRET: process.env.JWT_SECRET || 'super-secret-key'
-};
--- dist/modules/notifications/routes/notification.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notificationRouter = void 0;
-// src/modules/notification/routes/notification.route.ts
-const express_1 = require("express");
-const notification_controller_1 = require("../controllers/notification.controller");
-const router = (0, express_1.Router)();
-exports.notificationRouter = router;
-router.post('/send', notification_controller_1.notify);
