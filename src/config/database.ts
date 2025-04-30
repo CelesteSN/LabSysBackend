faff968b9c4f92d0ccf4518fa9bc72475bdc1466
Celeste <celesn3045@gmail.com>
2025-04-30 17:32:32 -0300

    fix: database

--- src/config/database.ts
+++ src/config/database.ts
@@ -20,5 +20,5 @@ console.log('DATABASE_URL:', process.env.DATABASE_URL);
 export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
   protocol: 'postgres',
-  logging: false,
+  //logging: false,
 });
