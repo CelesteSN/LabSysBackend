04c63239154cc79c4487926abafbf8c5aaa9f014
Celeste <celesn3045@gmail.com>
2025-04-30 17:14:54 -0300

    fix:database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,21 +1,24 @@
-import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize(
-  process.env.DB_NAME || 'pps_db',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'celeste123',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
-
 // import { Sequelize } from 'sequelize';
 
-// export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   logging: false,
-// });
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'pps_db',
+//   process.env.DB_USER || 'postgres',
+//   process.env.DB_PASSWORD || 'celeste123',
+//   {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: Number(process.env.DB_PORT) || 5432,
+//     dialect: 'postgres',
+//     logging: false,
+//   }
+// );
+
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+});
